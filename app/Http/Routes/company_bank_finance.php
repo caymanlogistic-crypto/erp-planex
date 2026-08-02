88721c126ed166e9f9414ea7e1b0cc80cafe19df
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-02 23:08:09 +0300

    feat: add manual bank statement mail refresh route

--- app/Http/Routes/company_bank_finance.php
+++ app/Http/Routes/company_bank_finance.php
@@ -7,6 +7,7 @@ $controller = new \App\Http\Controllers\Company\BankFinanceController($config, $
 $router->get('/company/finance/bank-accounts', [$controller, 'index']);
 $router->get('/company/bank-statement-settings', [$controller, 'bankStatementSettings']);
 $router->post('/company/finance/bank-accounts/import', [$controller, 'import']);
+$router->post('/company/finance/bank-accounts/refresh-from-mail', [$controller, 'refreshFromMail']);
 $router->post('/company/finance/bank-accounts/settings', [$controller, 'settings']);
 $router->post('/company/finance/bank-accounts/import/delete', [$controller, 'deleteImport']);
 $router->get('/company/finance/bank-accounts/reconciliation/json', [$controller, 'reconciliationJson']);
