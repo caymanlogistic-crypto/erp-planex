2d7de089494e6f9b499c16c8e32f0d82b2e15b94
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-12 08:25:29 +0000

    P39: reconcile historical migration checksums

--- app/Service/LocalMigrationService.php
+++ app/Service/LocalMigrationService.php
@@ -350,6 +350,18 @@ final class LocalMigrationService
                 'f0b9579f3d440d165465010e8693efbb5958dbc730253f53e0ca5b3a8733c4f3',
                 '7d5e4768f5cdba4008facf9b394b51f8980b33c0d2076aa56bf0b2cdba15d8f7',
             ],
+            '027_vehicle_unit_document_types.sql' => [
+                'efa2e433b05ce1e2a000b175e3925590a48ccef8f8ac47d5f6c241651c778fbd',
+            ],
+            '039_add_soft_delete_columns.sql' => [
+                '34524bfbfb0a8f83232c2076e0d1081d10c8eedc38aa5f6a69e84d2b43948038',
+            ],
+            '040_create_linear_routes.sql' => [
+                '88586161f063688f6f1a0133e200a237e2f86320fab1d42335375d7f5ef1bb1f',
+            ],
+            '041_linear_route_repeatable_principals_payments.sql' => [
+                '4d29448de5084d46a430b756ea2709baa9d7a77076734df989627546a0a3925f',
+            ],
             '051_restrict_finance_invoice_links_fk.sql' => [
                 '2afc6d44bdcf704192e7a6f9c279cfef302ef1da1f98aa2f485ee88837c6f834',
                 'c5e8053a9fc5866780b83be7465aec6c7149b074c18954dc6bbbba0f4b7a174a',
