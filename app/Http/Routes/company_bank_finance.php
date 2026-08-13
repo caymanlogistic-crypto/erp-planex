9d3184eb1f01d965f4ef11c0dbf0f97a0f15432c
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-14 01:26:21 +0300

    finance: route manual classification

--- app/Http/Routes/company_bank_finance.php
+++ app/Http/Routes/company_bank_finance.php
@@ -1,13 +1,12 @@
 <?php
-
 require_once base_path('app/Http/Controllers/Company/BankFinanceController.php');
-
-$controller = new \App\Http\Controllers\Company\BankFinanceController($config, $db);
-
-$router->get('/company/finance/bank-accounts', [$controller, 'index']);
-$router->get('/company/bank-statement-settings', [$controller, 'bankStatementSettings']);
-$router->post('/company/finance/bank-accounts/import', [$controller, 'import']);
-$router->post('/company/finance/bank-accounts/refresh-from-mail', [$controller, 'refreshFromMail']);
-$router->post('/company/finance/bank-accounts/settings', [$controller, 'settings']);
-$router->post('/company/finance/bank-accounts/import/delete', [$controller, 'deleteImport']);
-$router->get('/company/finance/bank-accounts/reconciliation/json', [$controller, 'reconciliationJson']);
+$controller=new \App\Http\Controllers\Company\BankFinanceController($config,$db);
+$router->get('/company/finance/bank-accounts',[$controller,'index']);
+$router->get('/company/bank-statement-settings',[$controller,'bankStatementSettings']);
+$router->post('/company/finance/bank-accounts/import',[$controller,'import']);
+$router->post('/company/finance/bank-accounts/refresh-from-mail',[$controller,'refreshFromMail']);
+$router->post('/company/finance/bank-accounts/settings',[$controller,'settings']);
+$router->post('/company/finance/bank-accounts/import/delete',[$controller,'deleteImport']);
+$router->get('/company/finance/bank-accounts/reconciliation/json',[$controller,'reconciliationJson']);
+$router->get('/company/finance/bank-transactions/{id}/classify',[$controller,'classifyForm']);
+$router->post('/company/finance/bank-transactions/{id}/classify',[$controller,'classifySubmit']);
