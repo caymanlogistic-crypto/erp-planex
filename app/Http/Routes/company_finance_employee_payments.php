ac995cabf779f25359565307ad29d6d564ec45c8
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-15 01:40:00 +0300

    finance: route employee movement correction

--- app/Http/Routes/company_finance_employee_payments.php
+++ app/Http/Routes/company_finance_employee_payments.php
@@ -5,5 +5,6 @@ $router->get('/company/finance/employee-payments', [$controller, 'index']);
 $router->get('/company/finance/employee-payments/create', [$controller, 'createForm']);
 $router->post('/company/finance/employee-payments/create', [$controller, 'createSubmit']);
 $router->get('/company/finance/employee-payments/employee/{id}', [$controller, 'employeeDetail']);
+$router->post('/company/finance/employee-payments/movements/{id}/reassign', [$controller, 'reassignMovement']);
 $router->post('/company/finance/employee-payments/bank-link', [$controller, 'bankLink']);
 $router->post('/company/finance/employee-payments/bank-unlink', [$controller, 'bankUnlink']);
