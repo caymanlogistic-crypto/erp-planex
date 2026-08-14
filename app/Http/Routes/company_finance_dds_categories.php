699a3f13d0ed1c50b834f7b0e7a4fcb902c91beb
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-14 13:19:18 +0300

    finance: route financial structure actions

--- app/Http/Routes/company_finance_dds_categories.php
+++ app/Http/Routes/company_finance_dds_categories.php
@@ -4,9 +4,13 @@ require_once base_path('app/Http/Controllers/Company/FinanceDdsCategoryControlle
 
 $controller = new \App\Http\Controllers\Company\FinanceDdsCategoryController($config, $db);
 
+// Kept at the existing URL for backward compatibility; UI label is now Financial Structure.
 $router->get('/company/finance/settings/dds-categories', [$controller, 'index']);
 $router->get('/company/finance/settings/dds-categories/create', [$controller, 'createForm']);
 $router->post('/company/finance/settings/dds-categories/create', [$controller, 'createSubmit']);
 $router->get('/company/finance/settings/dds-categories/edit', [$controller, 'editForm']);
 $router->post('/company/finance/settings/dds-categories/edit', [$controller, 'editSubmit']);
 $router->post('/company/finance/settings/dds-categories/active-toggle', [$controller, 'activeToggle']);
+$router->post('/company/finance/settings/dds-categories/cfu/save', [$controller, 'cfuSave']);
+$router->post('/company/finance/settings/dds-categories/cfu/toggle', [$controller, 'cfuToggle']);
+$router->post('/company/finance/settings/dds-categories/links/save', [$controller, 'linksSave']);
