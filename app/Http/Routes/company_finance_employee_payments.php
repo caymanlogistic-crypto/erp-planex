c49c54925f324978147b82c03a0111cc08c00de2
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-15 00:05:01 +0300

    finance: route employee payments

---
+++ app/Http/Routes/company_finance_employee_payments.php
@@ -0,0 +1,9 @@
+<?php
+require_once base_path('app/Http/Controllers/Company/FinanceEmployeePaymentsController.php');
+$controller = new \App\Http\Controllers\Company\FinanceEmployeePaymentsController($config, $db);
+$router->get('/company/finance/employee-payments', [$controller, 'index']);
+$router->get('/company/finance/employee-payments/create', [$controller, 'createForm']);
+$router->post('/company/finance/employee-payments/create', [$controller, 'createSubmit']);
+$router->get('/company/finance/employee-payments/employee/{id}', [$controller, 'employeeDetail']);
+$router->post('/company/finance/employee-payments/bank-link', [$controller, 'bankLink']);
+$router->post('/company/finance/employee-payments/bank-unlink', [$controller, 'bankUnlink']);
