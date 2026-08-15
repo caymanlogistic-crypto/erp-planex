d688398fb01311e866280a3daecd44b06d300cef
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-15 16:26:42 +0300

    feat: route technical cash employee dispatch

--- app/Http/Routes/company_finance_cash.php
+++ app/Http/Routes/company_finance_cash.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once base_path('app/Http/Controllers/Company/FinanceCashController.php');
+use App\Http\Controllers\Company\FinanceCashController;
 
-$controller = new \App\Http\Controllers\Company\FinanceCashController($config, $db);
+$financeCashController = new FinanceCashController($config, $db);
 
-$router->get('/company/finance/cash', [$controller, 'index']);
-$router->get('/company/finance/cash/account-create', [$controller, 'accountCreateForm']);
-$router->post('/company/finance/cash/account-create', [$controller, 'accountCreateSubmit']);
-$router->get('/company/finance/cash/operation-create', [$controller, 'operationCreateForm']);
-$router->post('/company/finance/cash/operation-create', [$controller, 'operationCreateSubmit']);
-$router->get('/company/finance/cash/transfer-create', [$controller, 'transferCreateForm']);
-$router->post('/company/finance/cash/transfer-create', [$controller, 'transferCreateSubmit']);
+$router->get('/company/finance/cash', [$financeCashController, 'index']);
+$router->get('/company/finance/cash/account-create', [$financeCashController, 'accountCreateForm']);
+$router->post('/company/finance/cash/account-create', [$financeCashController, 'accountCreateSubmit']);
+$router->get('/company/finance/cash/operation-create', [$financeCashController, 'operationCreateForm']);
+$router->post('/company/finance/cash/operation-create', [$financeCashController, 'operationCreateSubmit']);
+$router->get('/company/finance/cash/transfer-create', [$financeCashController, 'transferCreateForm']);
+$router->post('/company/finance/cash/transfer-create', [$financeCashController, 'transferCreateSubmit']);
+$router->post('/company/finance/cash/dispatch-employee', [$financeCashController, 'dispatchEmployeeSubmit']);
