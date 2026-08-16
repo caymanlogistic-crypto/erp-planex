608498d24d65825c7b5584d17b8ffe6b351dad8c
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-16 15:30:30 +0300

    Add employee transfer route

--- app/Http/Routes/company_finance_employee_payments.php
+++ app/Http/Routes/company_finance_employee_payments.php
@@ -4,6 +4,7 @@ $controller = new \App\Http\Controllers\Company\FinanceEmployeePaymentsControlle
 $router->get('/company/finance/employee-payments', [$controller, 'index']);
 $router->get('/company/finance/employee-payments/create', [$controller, 'createForm']);
 $router->post('/company/finance/employee-payments/create', [$controller, 'createSubmit']);
+$router->post('/company/finance/employee-payments/transfer', [$controller, 'transferSubmit']);
 $router->get('/company/finance/employee-payments/employee/{type}/{id}', [$controller, 'employeeDetail']);
 $router->post('/company/finance/employee-payments/movements/{id}/reassign', [$controller, 'reassignMovement']);
 $router->post('/company/finance/employee-payments/bank-link', [$controller, 'bankLink']);
