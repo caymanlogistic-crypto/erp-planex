e43d5c351c609dcabb8f0db7662a5c3485b925b3
caymanlogistic-crypto <cayman.logistic@gmail.com>
2026-08-17 00:49:14 +0300

    Load bank invoice settlement service

--- app/Support/entrypoint_dependencies.php
+++ app/Support/entrypoint_dependencies.php
@@ -38,6 +38,7 @@ require_once base_path('app/Service/CompanyDeletionService.php');
 require_once base_path('app/Service/FinanceAllocationService.php');
 require_once base_path('app/Service/FinanceInvoiceService.php');
 require_once base_path('app/Service/FinanceObligationService.php');
+require_once base_path('app/Service/FinanceBankInvoiceSettlementService.php');
 require_once base_path('app/Service/FinanceBalanceService.php');
 require_once base_path('app/Service/FinanceBankReconciliationService.php');
 require_once base_path('app/Service/FinanceBankReconciliationCutoffService.php');
